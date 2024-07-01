84d058ae360c13b7c5938b6fc26d6a0d91e48d5c
Mahesh M Nimkar <m.nimkar070@gmail.com>
2024-07-01 23:33:05 +0530

    Done

--- src/components/XCounter.js
+++ src/components/XCounter.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const XCounter = () => {
+    const [value , setValue] = useState(0);
+
+    const handleIncrement =()=>{
+        setValue((prev)=> prev+1)
+    }
+    const handleDecrement =()=>{
+        setValue((prev)=> prev-1)
+    }
+
   return (
     <div>
-      
+      <h1>Counter App</h1>
+      <p>Count: {value}</p>
+      <button type='button' onClick={handleIncrement}>Increment</button>
+      <button type='button' onClick={handleDecrement}>Decrement</button>
     </div>
   )
 }
