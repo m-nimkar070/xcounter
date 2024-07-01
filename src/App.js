f10aeccea07e6a7a9b14b7b082d7143f861b68b5
Mahesh M Nimkar <m.nimkar070@gmail.com>
2024-07-01 23:18:26 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import XCounter from './components/XCounter';
+
+function App() {
+  return (
+    <div className="App">
+      <XCounter />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/XCounter.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const XCounter = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default XCounter
